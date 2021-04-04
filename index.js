2004a63101e47d1788c6a57d47fe9f2aa30d22a3
github username <github email address>
2021-04-04 01:20:31 -0400

    worked with destructive and nondestructive functions on arrays

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+const driver = {}
+
+function updateDriverWithKeyAndValue(driver, key, value){
+    const newDrivers = {...driver};
+    
+    newDrivers[key] = value;
+
+    return newDrivers;
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
+     driver[key] = value;
+
+     return driver;
+}
+
+function deleteFromDriverByKey(driver, key){
+    const newDrivers = {...driver};
+
+    delete newDrivers[key];
+
+    return newDrivers;
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key){
+    delete driver[key];
+
+    return driver;
+}
